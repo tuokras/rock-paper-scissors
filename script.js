b841c63da3c9bac566a52beac64cf280921e7019
tuokras <tuokras@gmail.com>
2022-12-31 18:36:33 +0200

    Add ui to rps

--- script.js
+++ script.js
@@ -44,27 +44,24 @@ function checkPlayerWin(playerSelection, computerSelection) {
 }
 
 function playRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    console.log("Player selection: " + playerSelection);
-    console.log("Computer selection: " + computerSelection);
-
-    if (!(playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors")) {
-        return "Invalid entry";
-    }
+    document.querySelector("#player").textContent = "Player selection: " + playerSelection;
+    document.querySelector("#computer").textContent = "Computer selection: " + computerSelection;
 
     if (playerSelection === computerSelection) {
-        return "Draw!";
+        document.querySelector("#output").textContent = "Draw!";
+        return 2;
     }
 
     let playerWins = checkPlayerWin(playerSelection, computerSelection)
-
     if (playerWins) {
-        return ("You win! " + playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection);
+        document.querySelector("#output").textContent = ("You win! " + playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection);
+        return 1;
     } else {
-        return ("You lose! " + computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection);
+        document.querySelector("#output").textContent = ("You lose! " + computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection);
+        return 0;
     }
 }
 
@@ -72,17 +69,44 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     let playerSelection;
     let computerSelection;
-    let round = 0;
+    let playerScore = 0;
+    let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        round++;
-        console.log("Round " + round)
-        playerSelection = prompt("Enter your choice");
-        computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (playerScore < 5) {
+                playerSelection = button.id;
+                computerSelection = getComputerChoice();
+
+                switch (playRound(playerSelection, computerSelection)) {
+                    case 2:
+                        break;
+
+                    case 1:
+                        playerScore++;
+                        break;
+
+                    case 0:
+                        computerScore++;
+                        break;
+                }
+
+                console.log(playerScore);
+                document.querySelector("#score").textContent = "Player score: " + playerScore;
+
+                if (playerScore === 5) {
+                    if (playerScore < computerScore) {
+                        document.querySelector("#winner").textContent = "Winner: computer";
+                    } else {
+                        document.querySelector("#winner").textContent = "Winner: player";
+                    }
+                }
+            }
+        });
+    });
 }
 
 game();
-
 
